13928443b485d12414bd279e384d369ad8cb05ec
KevinJeanMarie <kjeanmarie01@gmail.com>
2024-06-25 00:53:00 +0200

    debugg app (display)

---
+++ Aforp-PM/metro.config.js
@@ -0,0 +1,13 @@
+/**
+ * Metro configuration for React Native
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @format
+ */
+
+module.exports = {
+    resolver: {
+      sourceExts: ['jsx', 'js', 'ts', 'tsx'] // Ajoutez les extensions que vous utilisez dans votre projet
+    }
+  };
+  
