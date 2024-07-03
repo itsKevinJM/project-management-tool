424540d52910546663206c7c6d26999c3274830a
kayembejordy <kayembe.jordy@gmail.com>
2024-07-03 13:10:46 +0200

    modification fond d'écran

--- Aforp-PM/components/TaskBoard.tsx
+++ Aforp-PM/components/TaskBoard.tsx
@@ -1,21 +1,31 @@
-import react from 'react';
-import { View, StyleSheet, SafeAreaView } from 'react-native';
+import React from 'react';
+import { View, StyleSheet, SafeAreaView, ImageBackground } from 'react-native';
 import TaskList from './TaskList';
 import { LinearGradient } from 'expo-linear-gradient';
-
+import Monfond from '../assets/images/fond-ecran-wallpaper-noir.png';
 
 export default function TaskBoard() {
     return (
-        <View style={{ padding: 10, flex: 1 }}>
+        <ImageBackground source={Monfond} style={styles.backgroundImage}>
             <LinearGradient
                 // Background Linear Gradient
-                colors={['pink','orange', 'purple', 'blue']}
+                colors={['rgba(255, 182, 193, 0.5)', 'rgba(255, 140, 0, 0.5)', 'rgba(128, 0, 128, 0.5)', 'rgba(0, 0, 255, 0.5)']}
                 style={StyleSheet.absoluteFill}
             />
-            <SafeAreaView>
+            <SafeAreaView style={styles.safeArea}>
                 <TaskList /> 
             </SafeAreaView>
-        </View>
+        </ImageBackground>
     );
 }
 
+const styles = StyleSheet.create({
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    safeArea: {
+        flex: 1,
+        padding: 10,
+    },
+});
